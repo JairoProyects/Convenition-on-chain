7de01af35771d7c5617c5a2bc0825540267e8be3
Luiz Torres Villalobos <112789200+luiztorres2002@users.noreply.github.com>
2025-05-16 10:50:15 -0600

    Structure Update

--- Backend/src/main/java/com/zkbytebandits/convenio/onchain/ConvenioOnChainApplication.java
+++ Backend/src/main/java/com/zkbytebandits/convenio/onchain/ConvenioOnChainApplication.java
@@ -1,4 +1,4 @@
-package com.Product.Convenio.onChain;
+package com.zkbytebandits.convenio.onchain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
