630a7d54b72f1bc63383f55aa86e57dea4eaecea
Luiz Torres Villalobos <112789200+luiztorres2002@users.noreply.github.com>
2025-05-16 01:40:11 -0600

    Creacion del backend SpringBoot

---
+++ Backend/src/test/java/com/Product/Convenio/onChain/ConvenioOnChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Product.Convenio.onChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenioOnChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
