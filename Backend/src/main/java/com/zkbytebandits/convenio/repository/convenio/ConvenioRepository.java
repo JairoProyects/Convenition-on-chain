705b50217d871bc78b1ceb4fe3170a11c61314e6
Luisby47 <75922735+Luisby47@users.noreply.github.com>
2025-05-23 21:33:41 -0600

    Convenio Files Updated

--- Backend/src/main/java/com/zkbytebandits/convenio/repository/convenio/ConvenioRepository.java
+++ Backend/src/main/java/com/zkbytebandits/convenio/repository/convenio/ConvenioRepository.java
@@ -1,14 +1,16 @@
 package com.zkbytebandits.convenio.repository.convenio;
 
 import com.zkbytebandits.convenio.entity.Convenio;
+import com.zkbytebandits.convenio.entity.Convenio.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ConvenioRepository extends JpaRepository<Convenio, Long> {
-    Optional<Convenio> findByExternalId(String externalId);
     Optional<Convenio> findByOnChainHash(String onChainHash);
+    List<Convenio> findByStatus(Status status);
     // Add other custom query methods if needed, for example, find by certain criteria
-} 
+}
